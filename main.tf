97ff611fd2bb81a0a41c4ed596383b10094560a1
Zhiyuan Li <lizhiyua@amazon.com>
2022-09-07 18:25:27 -0400

    updated main.tf in root

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 module "cicd_infra" {
   source = "./modules/cicd-infra"
 
-  codeartifact_repository_name = "APG_Artifact_Repo_lizhiyua"
-  codecommit_repo_name = "APG_App_Repo_lizhiyua"
-  build_project_name = "APG_Build_lizhiyua"
-  pipeline_name = "APG_Pipeline_lizhiyua"
+  aws_region = "us-east-2"
+  codeartifact_repository_name = "Artifact_Repo_Demo"
+  codecommit_repo_name = "App_Repo_Demoo"
+  build_project_name = "Build_Demo"
+  pipeline_name = "Pipeline_Demo"
+  domain_name = "Demo"
 }
